9c6009a01dc314b5b3baa95c04b39c5052be43f4
florianingelfinger <florian@ingelfinger.eu>
2023-12-04 12:16:37 +0200

    add model file

--- src/cytovi/__init__.py
+++ src/cytovi/__init__.py
@@ -1,7 +1,8 @@
 from importlib.metadata import version
 
 from . import pl, pp, tl
+from .models import CytoVI
 
-__all__ = ["pl", "pp", "tl"]
+__all__ = ["pl", "pp", "tl", "CytoVI", "CytoVAE"]
 
 __version__ = version("CytoVI")
