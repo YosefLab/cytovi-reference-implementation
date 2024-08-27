48e29e94b003e5e97574a74a26203b4e839c1ab3
florianingelfinger <florian@ingelfinger.eu>
2024-08-27 17:05:14 +0300

    add kwargs for biaxial plot

--- src/cytovi/pl/cyto_pl.py
+++ src/cytovi/pl/cyto_pl.py
@@ -118,6 +118,8 @@ def biaxial(
     n_obs: int = 10000,
     sample_color_groups: bool = False,
     save: Union[bool, str] = None,
+    kde_kwargs: dict = None,
+    scatter_kwargs: dict = None,
     **kwargs,
 ):
     """
@@ -187,8 +189,8 @@ def biaxial(
         data_plot[color] = adata.obs[color]
 
     g = sns.PairGrid(data_plot, x_vars=marker_x, y_vars=marker_y, hue=color, **kwargs)
-    g.map(sns.kdeplot, levels=n_bins)
-    g.map(sns.scatterplot, s=5)
+    g.map(sns.kdeplot, levels=n_bins, **kde_kwargs)
+    g.map(sns.scatterplot, s=5, **scatter_kwargs)
     g.add_legend()
 
     if save is not None:
